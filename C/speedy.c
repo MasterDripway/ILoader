8bd318c1126cccfe07a732c363b64eb6d1716b61
Manu1800 <79649658+Manu0811@users.noreply.github.com>
2021-09-07 11:38:39 -0400

    upd

--- C/speedy.c
+++ C/speedy.c
@@ -16,32 +16,3 @@ __declspec(dllexport) float speedy_sqrt(float num, float speedFactor) {
         }
         return n;
 }
-__declspec(dllexport) int * genArr() {
-    static int array[1024];;
-    for (int i=0; i < 1024; i++) {
-        array[i] = i;
-    }
-    return array;
-
-}
-int main() {
-    int arraysize = pow(10, 3);
-    printf("full comp size: %d\n", arraysize);
-    time_t t1, t2;
-    int *array;
-    double dt;
-    time(&t1);
-    /*
-    for (int i=0; i <= arraysize; i++) {
-        speedy_sqrt(i, 1);
-    }*/
-    array = genArr();
-    time(&t2);
-    dt = difftime(t1, t2);
-    printf("Time: %.2fs\n", dt);
-    printf("done.\n");
-    for (int x = 0; x < 1024; x++) {
-        printf("%d\n", array[x]);
-    }
-    Sleep(1000);
-}
